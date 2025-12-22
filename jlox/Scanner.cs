815fd65e7a5da15b0e71d1976ede02768dc0987b
Amara Manasa <bl.en.u4cse23006@bl.students.amrita.edu>
2025-12-22 07:17:58 +0530

    finished ch4 scanning

--- jlox/Scanner.cs
+++ jlox/Scanner.cs
@@ -48,8 +48,25 @@ public class Scanner{
                     addToken(TokenType.SLASH);
                 }
                 break;
+            case ' ':
+            case '\r':
+            case '\t':break;
+            case '\n': line++; break;
+            case '"': String(); break;
+            case 'o':
+                if (match('\r')){
+                    addToken(TokenType.OR);
+                }
+                break;
             default:
-                Lox.Error(line,"Unexpected Character.");
+                if (IsDigit(c)) {
+                    Number();
+                } else if (IsAlpha(c)){
+                    Identifier();
+                } 
+                else {
+                    Lox.Error(line, "Unexpected character.");
+                }
                 break;
         }
     }
@@ -63,7 +80,7 @@ public class Scanner{
     }
 
     private void addToken(TokenType type, object literal){
-        string text = source.Substring(start,current);
+        string text = source.Substring(start,current - start);
         tokens.Add(new Token(type, text, literal, line));
     }
 
@@ -78,4 +95,72 @@ public class Scanner{
         if (IsAtEnd()) return '\0';
         return source[current];
     }
+      
+    private void String(){
+        while (peek() != '"' && !IsAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+        if (IsAtEnd()) {
+            Lox.Error(line, "Unterminated string.");
+            return;
+        }
+        advance();
+        string value = source.Substring(start + 1, current - 1 - start);
+        addToken(TokenType.STRING, value);
+    }
+
+    private bool IsDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+
+    private void Number(){
+        while(IsDigit(peek())) advance();
+        if (peek() == '.' && IsDigit(PeekNext())){
+            advance();
+            while (IsDigit(peek())) advance();
+        }
+        addToken(TokenType.NUMBER,double.Parse(source.Substring(start, current - start)));
+    }
+
+    private char PeekNext(){
+        if (current + 1 >= source.Length) return'\0';
+        return source[current + 1];
+    }
+
+    private void Identifier(){
+        while(IsAlphaNumeric(peek())) advance();
+        string text = source.Substring(start, current - start);
+        TokenType type = keywords.TryGetValue(text, out var t) ? t : TokenType.IDENTIFIER;
+        addToken(type);
+    }
+
+    private bool IsAlpha(char c){
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '-';
+    }
+
+    private bool IsAlphaNumeric(char c){
+        return IsAlpha(c) || IsDigit(c);
+    }
+
+    private static readonly Dictionary<string, TokenType> keywords =
+        new()
+        {
+            ["and"]    = TokenType.AND,
+            ["class"]  = TokenType.CLASS,
+            ["else"]   = TokenType.ELSE,
+            ["false"]  = TokenType.FALSE,
+            ["for"]    = TokenType.FOR,
+            ["fun"]    = TokenType.FUN,
+            ["if"]     = TokenType.IF,
+            ["nil"]    = TokenType.NIL,
+            ["or"]     = TokenType.OR,
+            ["print"]  = TokenType.PRINT,
+            ["return"] = TokenType.RETURN,
+            ["super"]  = TokenType.SUPER,
+            ["this"]   = TokenType.THIS,
+            ["true"]   = TokenType.TRUE,
+            ["var"]    = TokenType.VAR,
+            ["while"]  = TokenType.WHILE
+        };
 }
