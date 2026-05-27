e47a35f29fedebdf7f47a54d05bfee94ae67096a
Amara Manasa <bl.en.u4cse23006@bl.students.amrita.edu>
2026-05-27 20:07:19 +0530

    finished till 18.2

--- clox/Value.cs
+++ clox/Value.cs
@@ -1,32 +1,78 @@
-using Value = System.Double;
+public enum ValueType{
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+}
+
+public struct Value{
+    public ValueType Type;
+
+    public bool Boolean;
+    public double Number;
+    public bool IsBool => Type == ValueType.VAL_BOOL;
+    public bool IsNil => Type == ValueType.VAL_NIL;
+    public bool IsNumber => Type == ValueType.VAL_NUMBER;
+    public bool AsBool => Boolean;
+    public double AsNumber => Number;
+    public static Value BoolVal(bool value){
+        return new Value{
+            Type = ValueType.VAL_BOOL,
+            Boolean = value
+        };
+    }
+
+    public static Value NumberVal(double value){
+        return new Value{
+            Type = ValueType.VAL_NUMBER,
+            Number = value
+        };
+    }
+
+    public static Value NilVal(){
+        return new Value{
+            Type = ValueType.VAL_NIL
+        };
+    }
+}
+
 public class ValueArray{
     public int Capacity;
     public int Count;
     public Value[] Values;
 
     public static void InitValueArray(ValueArray array){
-        array.Values = null;
+        array.Values = Array.Empty<Value>();
         array.Capacity = 0;
         array.Count = 0;
     }
 
     public static void WriteValueArray(ValueArray array, Value value){
         if (array.Capacity < array.Count + 1){
-            int OldCapacity = array.Capacity;
-            array.Capacity = Memory.GrowCapacity(OldCapacity);
-            array.Values = Memory.GrowArray<Value>(array.Values, OldCapacity, array.Capacity);
+            int oldCapacity = array.Capacity;
+            array.Capacity = Memory.GrowCapacity(oldCapacity);
+            Array.Resize(ref array.Values, array.Capacity);
         }
         array.Values[array.Count] = value;
         array.Count++;
     }
 
     public static void FreeValueArray(ValueArray array){
-        Memory.FreeArray<Value>(ref array.Values);
-        InitValueArray(array);
+        array.Values = Array.Empty<Value>();
+        array.Capacity = 0;
+        array.Count = 0;
     }
 
-    public static void PrintValue(Value value)
-    {
-        Console.Write(value);
+    public static void PrintValue(Value value){
+        switch (value.Type){
+            case ValueType.VAL_BOOL:
+                Console.Write(value.Boolean ? "true" : "false");
+                break;
+            case ValueType.VAL_NIL:
+                Console.Write("nil");
+                break;
+            case ValueType.VAL_NUMBER:
+                Console.Write(value.Number);
+                break;
+        }
     }
 }
